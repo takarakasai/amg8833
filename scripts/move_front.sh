6ce2ca265717d6d376cf41976a43d405ca923a92
takarakasai <takara.kasai@gmail.com>
2019-03-22 01:34:18 +0900

    add test scripts.

---
+++ scripts/move_front.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sleep 3
+
+rostopic pub -1 /cmd_vel geometry_msgs/Twist "linear:
+  x: 0.3
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0"
+
+rostopic pub -1 /cmd_vel geometry_msgs/Twist "linear:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0"
+
